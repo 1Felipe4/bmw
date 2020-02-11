63ec665d46086deb2627371f2732de28a6369065
1Felipe4 <32851789+1Felipe4@users.noreply.github.com>
2020-02-10 22:56:16 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,240 +1,148 @@
-function item(name, display, price){
-  this.name = name;
-  this.display = display;
-  this.price = price;
-}
+var canvas = [document.getElementById("knob1"),document.getElementById("knob2")];
+var burner = [document.getElementById("burner"), document.getElementById("small-burner")];
 
+function draw(){
+  for (var i = 0; i < canvas.length; i++) {
+    var ctx = canvas[i].getContext("2d");
+    var radius = canvas[i].height / 2;
+    ctx.translate(radius, radius);
+    radius = radius * 0.90
+    drawKnob(ctx, radius)
+    drawHand(ctx, ang, 40, 6)
+  }
 
-function car(type, design, color, rimType, interior){
-
-  this.type = type;
-  this.design = design;
-  this.color = color;
-  this.rimType = rimType;
-  this.interior = interior;
-
-    this.designOptions = function(){
-      if(this.type=== "x7"){
-        return ["luxury", "mSport"];
-      }
-      else{
-        return["xLine", "mSport"]
-      }
-    }
-
-    this.colorOptions= function(){
-      if(this.type ==="x7"){
-        return["white", "blue", "amtrin"];
-        }
-      else if(this.type ==="x1"){
-        if(this.design === "xLine"){
-          return ["white", "blue", "orange"];
-        }else{
-          return ["white", "blue", "grey"];
-        }
-      }
-      else if(this.type === "x4"){
-        return["white", "black", "red"]
-      }
-      }
-    this.interiorOptions= function(){
-        if(this.type ==="x7"){
-          return["white", "black", "coffee"];
-          }
-          else if(this.type ==="x1"){
-            return["white", "black", "mocha"];
-            }
-            else if(this.type === "x4"){
-              return["black","mocha","red"]
-            }
-        }
-
-    this.rimOptions= function(){
-      if(this.type ==="x7"){
-        if(this.design ==="luxury"){
-            return["21-Y-ferric", "22-V", "22-multi"]
-          }
-          else{
-            return["21-dbl", "22-V", "22-Y"]
-          }
-        }
-        else if(this.type ==="x1"){
-          if(this.design ==="xLine"){
-            return["18-Y", "19-Y", "18-dbl"]
-          }
-          else{
-            return["18-M", "18-M", "19-M"]
-          }
-        }
-        else if(this.type ==="x4"){
-          if(this.design ==="xLine"){
-            return["19-Y-ferric","19-Y-ferric","20-Y-bicolor"]
-          }
-          else{
-            return["19-M-bicolor","19-M-bicolor" ,"20-M-bicolor"];
-          }
-        }
-
-    }
-    this.changeDesign = function(d){
-          var dO = this.designOptions();
-          let j = 0;
-          for(var i = 0; i<dO.length;i++){
-          if(d===dO[i]){this.design = d;}
-          else{j++};
-        }
-        if(j==2){
-          this.design = dO[0];
-        }
-        this.changeColor(this.color);
-        this.changeRim(this.rimType);
-      this.designImg();
-    }
-    this.changeColor = function(c){
-      var cO = this.colorOptions();
-      let j = 0;
-      for(var i = 0; i < cO.length;i++){
-        if(c===cO[i]){
-          this.color = c;
-
-        }  else{
-          j++;
-        }
-      }
-      if(j === 3){
-        this.color = cO[0]
-      }
-      this.colorImg("cPic");
-    }
-    this.changeRim = function(r){
-      var rO = this.rimOptions();
-      let j = 0;
-      for(var i = 0; i < rO.length;i++){
-        if(r==rO[i]){
-          this.rimType = r;
-        }else{
-          j++;
-        }
-      }
-      if(j === 3){
-        this.rimType = rO[0]
-      }
-      this.colorImg("cPic");
-    }
-
-    this.changeInterior = function(n){
-      var inO = this.interiorOptions();
-      let j = 0;
-      for(let i = 0; i < inO.length;i++){
-        if(n==inO[i]){
-          this.interior = n;
-        }else{
-          j++;
-        }
-      }
-      if(j === 3){
-        this.interior = inO[0];
-      }
-      this.interiorImg("iPic");
-    }
-
-    this.colorImg = function(){
-    document.getElementById("cPic").src=("images/" +this.type + "-" + this.design + "-"  + this.color + "-" + this.rimType + ".png")
-    setButtons();
-    return "images/" +this.type + "-" + this.design + "-"  + this.color + "-" + this.rimType + ".png"};
-    this.designImg = function(){
-    document.getElementById('dPic').src=("images/" + this.type + "-" +  this.design + ".png");
-    setButtons();
-    return this.type + "-" +  this.design+ ".png"};
-    this.interiorImg = function(){
-    document.getElementById('iPic').src=("images/" + this.type + "-interior-" + this.interior + ".png");
-    return this.type + "-interior-" + this.interior+ ".png"};
-
-    this.cookie = function(){
-      var d = new Date();
-      d.setTime(d.getTime()+(10*24*60*60*1000));
-      var expires = "expires=" + d.toUTCString();
-
-      document.cookie = ("type=" + this.type + ";"  +expires + ";");
-
-      document.cookie = ("design=" + this.design + ";"  +expires + ";");
-      document.cookie = ("color=" + this.color + ";"  +expires + ";");
-      document.cookie = ("rimType=" + this.rimType + ";"  +expires + ";");
-      document.cookie = ("interior=" + this.interior + ";"  +expires + ";");
-      document.cookie = ("expires=" + expires + ";" +expires + ";");
-      document.cookie = ("path=/;");
-      return"type=" + this.type + ";design=" +this.design+ ";color=" + this.color +
-      ";rimType=" + this.rimType + ";interior=" + this.interior + ";" + expires + ";path=/";
-
+  for (var i = 0; i < burner.length; i++) {
+    var ctx = burner[i].getContext("2d");
+    var radius = burner[i].height / 2;
+    ctx.translate(radius, radius);
+    radius = radius * 0.90;
+    drawBurner(ctx, radius, 'darkgray');
+    update(i+1);
   }
 
 }
 
-var x;
 
+function drawBurner(ctx, radius, color){
+  var grad;
+  ctx.beginPath();
+  ctx.arc(0, 0, radius, 0, 2*Math.PI);
+  ctx.fillStyle = '' + color;
+  ctx.fill();
+  grad = ctx.createRadialGradient(0,0,radius*0.95, 0,0,radius*1.05);
+  grad.addColorStop(0, '#333');
+  grad.addColorStop(0.5, 'white');
+  grad.addColorStop(1, '#333');
+  ctx.strokeStyle = grad;
+  ctx.lineWidth = radius*0.1;
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(0, 0, radius*0.1, 0, 2*Math.PI);
+  ctx.fillStyle = '#333';
+  ctx.fill();
+  drawLines(ctx, radius);
+}
 
-function carD(type){
-if(type=== "x7"){
-x =new car("x7", "luxury", "white", "21-Y-ferric", "black")
+function drawKnob(ctx, radius){
+  var grad;
+  ctx.beginPath();
+  ctx.arc(0, 0, radius, 0, 2*Math.PI);
+  ctx.fillStyle = 'gray';
+  ctx.fill();
+  grad = ctx.createRadialGradient(0,0,radius*0.95, 0,0,radius*1.05);
+  grad.addColorStop(0, '#333');
+  grad.addColorStop(0.5, 'white');
+  grad.addColorStop(1, '#333');
+  ctx.strokeStyle = grad;
+  ctx.lineWidth = radius*0.1;
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(0, 0, radius*0.1, 0, 2*Math.PI);
+  ctx.fillStyle = '#333';
+  ctx.fill();
+  drawNumbers(ctx, radius);
+  ang = -4 * Math.PI /6;
+
+}
+
+function drawNumbers(ctx, radius) {
+  var ang;
+  var num;
+  var i = -4;
+  ctx.font = radius * 0.15 + "px arial";
+  ctx.fillStyle = "ghostwhite";
+  ctx.textBaseline = "middle";
+  ctx.textAlign = "center";
+  for(num = 400; num >= 0; num-=50, i++){
+    ang = i * Math.PI /6;
+    ctx.rotate(-ang);
+    ctx.translate(0, -radius * 0.8);
+    ctx.rotate(ang);
+    ctx.fillText(num.toString(), 0, 0);
+    ctx.rotate(-ang);
+    ctx.translate(0, radius * 0.8);
+    ctx.rotate(ang);
   }
-else if (type ==="x1") {
-  x = new car("x1", "xLine", "white", "18-Y", "black")
-}
-else{
-  x = new car("x4", "xLine","white","18-M-bicolor", "black")
 }
 
-x.cookie();
+function drawLines(ctx, radius) {
+var ang;
+var num;
+ctx.font = radius * 0.15 + "px arial";
+ctx.textBaseline = "middle";
+ctx.textAlign = "center";
+for(num = 1; num < 13; num++){
+  ang = num * Math.PI / 6;
+  ctx.rotate(ang);
+  ctx.translate(0, -radius * 0.85);
+  ctx.rotate(-ang);
+  ctx.moveTo(0,0);
+  ctx.lineTo(10,5);
+  ctx.stroke();
+  ctx.rotate(ang);
+  ctx.translate(0, radius * 0.85);
+  ctx.rotate(-ang);
 }
 
-function rebuild(){
-    let type = getCookie("type");
-    let design = getCookie("design");
-    let color= getCookie("color");
-    let rimType = getCookie("rimType");
-    let interior = getCookie("interior");
-
-    x = new car(type, design, color, rimType, interior);
-    x.designImg();
-    x.colorImg();
-    x.interiorImg();
-
 }
 
-function setButtons(){
-  x.cookie();
-  let dO = x.designOptions();
-  document.getElementById("d1").innerHTML = dO[0];
-  document.getElementById("d2").innerHTML = dO[1];
 
-  let cO = x.colorOptions();
-  document.getElementById("c1").innerHTML = cO[0];
-  document.getElementById("c2").innerHTML = cO[1];
-  document.getElementById("c3").innerHTML = cO[2];
-
-  let rO = x.rimOptions();
-  document.getElementById("r1").innerHTML = rO[0];
-  document.getElementById("r2").innerHTML = rO[1];
-  document.getElementById("r3").innerHTML = rO[2];
-
-  let inO = x.interiorOptions();
-  document.getElementById("in1").innerHTML = inO[0];
-  document.getElementById("in2").innerHTML = inO[1];
-  document.getElementById("in3").innerHTML = inO[2];
+function drawHand(ctx, pos, length, width) {
+  ctx.beginPath();
+  ctx.lineWidth = width;
+  ctx.lineCap = "round";
+  ctx.moveTo(0,0);
+  ctx.rotate(pos);
+  ctx.lineTo(0, -length);
+  ctx.stroke();
+  ctx.rotate(-pos);
 }
 
-function getCookie(cname) {
-  var name = cname + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for(var i = 0; i <ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
+function update(i){
+
+  i = parseInt(i);
+  var pos = parseFloat(document.getElementById("range" + i).value);
+  i--;
+  var ctx =  canvas[i].getContext("2d");
+  var radius = canvas[i].height / 2;
+
+  drawKnob(ctx, radius);
+  drawHand(ctx, pos, 40, 6)
+
+  ctx = burner[i].getContext("2d");
+  radius = burner[i].height/2;
+  var color;
+  if(pos < 5){
+    color="darkgray";
   }
-  return "";
+  else if(pos < 7.4){
+    color="darkorange";
+  }
+  else{
+    color = "red";
+  }
+
+
+  drawBurner(ctx, radius, color);
+
 }
